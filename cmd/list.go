bbb5c64c77db000b30020096efde5f37eae30c52
Jason Larsen <jason.larsen@gmail.com>
2018-02-25 12:00:44 +1300

    basic adding/listing by description working!

--- cmd/list.go
+++ cmd/list.go
@@ -1,6 +1,12 @@
 package cmd
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/blevesearch/bleve"
 	. "github.com/spf13/cobra"
 )
 
@@ -10,6 +16,30 @@ var listCmd = &Command{
 	Short: "list images from the database",
 	Args:  MinimumNArgs(1),
 	Run: func(cmd *Command, args []string) {
+		queryString := strings.Join(args, " ")
+		query := bleve.NewQueryStringQuery(queryString)
+		searchRequest := bleve.NewSearchRequest(query)
+		index, initErr := initDB()
+		if initErr != nil {
+			log.Fatal(initErr)
+		}
+		results, err := index.Search(searchRequest)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, hit := range results.Hits {
+			uuid := hit.ID
+			var buf []byte
+			buf, err = index.GetInternal([]byte(uuid))
+			if err != nil {
+				log.Fatal(err)
+			}
+			image := Image{}
+			if err := json.Unmarshal(buf, &image); err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(image.Url)
+		}
 	},
 }
 
