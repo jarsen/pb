7e659046bde7042b6af64dedb561759a25ae396c
Jason Larsen <jason.larsen@gmail.com>
2018-03-06 23:34:09 -0700

    implement rm command

--- cmd/rm.go
+++ cmd/rm.go
@@ -1,17 +1,28 @@
 package cmd
 
 import (
-	"fmt"
+	"log"
 
+	"github.com/jarsen/pb/db"
 	. "github.com/spf13/cobra"
 )
 
-// rmCmd represents the rm command
 var rmCmd = &Command{
 	Use:   "rm ID",
 	Short: "removes an entry from the database",
+	Args:  ExactArgs(1),
 	Run: func(cmd *Command, args []string) {
-		fmt.Println("not yet implemented")
+		id := args[0]
+		index, err := db.Init()
+		if err != nil {
+			log.Fatal("Error loading database")
+		}
+		batch := index.NewBatch()
+		batch.Delete(id)
+		err = index.Batch(batch)
+		if err != nil {
+			log.Fatal("Error deleting from database")
+		}
 	},
 }
 
